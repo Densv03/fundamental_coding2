28a71c1c09dab0f966a6a41af31e82414c760ead
Denys <den34092den@gmail.com>
2020-12-11 19:34:26 +0300

    Lab_6 upload

---
+++ Lab_6/2/2.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<iostream>
+#include<ctime>
+int main() {
+	int comp_number, user_number, attempts = 0;
+	srand(time(0));
+	comp_number = rand() % 10 + 1;
+	//printf("%d\n", comp_number);
+	while (true) {
+		printf("Enter number: ");
+		scanf("%d", &user_number);
+		attempts++;
+		if (comp_number > user_number)printf("My number is more\n");
+		else if (comp_number < user_number)printf("My number is less\n");
+		else {
+			printf("Congratulations! It took %d attempts", attempts);
+			break;
+		}
+	}
+}
---
+++ Lab_6/3/3.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<ctime>
+#include<iostream>
+int num1, num2, ans, k, mistakes, mark, checker;
+int main() {
+	srand(time(0));
+	for (int i = 0; i < 10; ++i) {
+		num1 = rand() % 9 + 1;
+		num2 = rand() % 9 + 1;
+		printf("%d * %d = ", num1, num2);
+		//scanf("%d", &k);
+
+		checker = rand() % 10 + 1;
+		if (checker <= 8) {
+			k = num1 * num2;
+			printf("%d\n", k);
+		}
+		else {
+			k = rand() % 90 + 1;
+			printf("%d\n", k);
+		}
+
+		if (num1 * num2 != k)mistakes++;
+	}
+	if (mistakes > 4)mark = 1;
+	else mark = 5 - mistakes;
+	printf("Your mark is %d", mark);
+}
