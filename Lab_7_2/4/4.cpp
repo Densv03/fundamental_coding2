646ab8aa083d58971324160a3d8e01fa6190e62d
Den <den34092den@gmail.com>
2021-02-07 10:08:17 +0300

    backup

---
+++ Lab_7_2/4/4.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+int main(){
+    srand(time(0));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << 1;
+    FILE* fp;
+    int temp, f = 1214134;
+    if ((fp = fopen("text.txt", "wb")) == NULL) {
+        printf("Error");
+        return 1;
+    }
+    fwrite(&f, sizeof(float), 1, fp);
+}
