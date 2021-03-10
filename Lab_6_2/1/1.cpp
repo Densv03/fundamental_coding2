0c94d12e9cdbeed4cead7efa03163beeaaf7c9aa
Den <den34092den@gmail.com>
2021-03-10 22:58:00 +0200

    fixed: Lab_6_2

--- Lab_6_2/1/1.cpp
+++ Lab_6_2/1/1.cpp
@@ -2,30 +2,37 @@
 #include<iostream>
 #include<ctime>
 #include<Windows.h>
+#define N 100
 using namespace std;
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	srand(time(0));
-	string s;
+	char s[N];
 	int checker, size;
 	char symbol;
 	cout << "Введите строку: " << endl << "> ";
 	cin >> s;
-	while (s[0] != s[s.size() - 1] || s.size()==1) {
+	char* pointer;
+	pointer = s;
+	while (*s != *(s+ strlen(s) - 1) || strlen(s)==1) {
 		cout << "Введена некорректная строка (Первый и последний символы должны совпадать, строка должна иметь больше одного символа). Повторите ввод:" << endl<<"> ";
 		cin >> s;
 	}
 	cout << "Введите символ который будет удалён: " << endl << "> ";
 	cin >> symbol;
-	while (symbol != s[0]) {
-		cout << "Введён некорректный символ (Символ должен присутствовать в строке). Повторите ввод:" << endl << "> ";
+	while (symbol != *s) {
+		cout << "Введён некорректный символ (Символ должен присутствовать в строке и быть первым и последним элементом). Повторите ввод:" << endl << "> ";
 		cin >> symbol;
 	}
-	s.erase(0, 1);
-	s.erase(s.size() - 1, 1);
-	if (s.empty())
+	char ans[N];
+	for (int i = 0; i < N; ++i)
+		*(ans + i) = '\0';
+	for (int i = 1; i < strlen(s) - 1; ++i)
+		*(ans + i - 1) = *(s + i);
+	if (!strlen(ans))
 		cout << "Строка пуста";
 	else
-		cout << s;
+		for (int i = 0; i < strlen(ans); ++i)
+			cout << *(ans + i);
 }
